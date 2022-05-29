26a86db13cd909659f1d7764cf5b62188906d3f2
River <riveriswild@icloud.com>
2022-05-29 03:40:18 +0300

    API Tests + frontend

--- tweets/tests.py
+++ tweets/tests.py
@@ -10,9 +10,10 @@ User = get_user_model()
 class TweetTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create(username="cfe", password="somepassword")
+        self.userb = User.objects.create(username="cfe-2", password="somepassword2")
         Tweet.objects.create(content="my first tweet", user=self.user)
         Tweet.objects.create(content="my first tweet", user=self.user)
-        Tweet.objects.create(content="my first tweet", user=self.user)
+        Tweet.objects.create(content="my first tweet", user=self.userb)
         self.currentCount = Tweet.objects.all().count()
         self.client = APIClient()
 
@@ -59,7 +60,7 @@ class TweetTestCase(TestCase):
         self.client.force_authenticate(self.user)
         response = self.client.post("/api/tweets/action/",
                                {"id": 1, "action": "like"})
-        print('tets', response.json())
+        #print('tets', response.json())
         self.assertEqual(response.status_code, 200)
         like_count = response.json().get("likes")
         self.assertEqual(like_count, 1)
@@ -81,20 +82,18 @@ class TweetTestCase(TestCase):
         like_count = response.json().get("likes")
         self.assertEqual(like_count, 0)
 
-
-    def test_action_retweet(self):    # TODO ValueError: Content-Type header is "text/html", not "application/json"
-
+    def test_action_retweet(self):
         # client = self.get_client()
         self.client.force_authenticate(self.user)
         current_count = self.currentCount
-        response = self.client.post("api/tweets/action/",
-                               {"id": 2, "action": "retweet"})
-        #self.assertEqual(response.status_code, 201)
+        response = self.client.post("/api/tweets/action/",
+                               {"id": 3, "action": "retweet"})
+        #print("RESPONSE", response)
+        self.assertEqual(response.status_code, 201)
         data = response.json()
         new_tweet_id = data.get("id")
         self.assertNotEqual(2, new_tweet_id)
-        self.assertEqual((current_count + 1, new_tweet_id))
-
+        self.assertEqual(current_count + 1, new_tweet_id)
 
     def test_tweet_create_api_view(self):
         self.client.force_authenticate(self.user)
@@ -102,8 +101,28 @@ class TweetTestCase(TestCase):
         #client = self.get_client()
         # print(client._credentials)
         response = self.client.post("/api/tweets/create/", request_data)
-        print(response.data)
         self.assertEqual(response.status_code, 201)
         response_data = response.json()
         new_tweet_id = response_data.get("id")
-        #self.assertEqual(self.currentCount + 1, new_tweet_id)
+        self.assertEqual(self.currentCount + 1, new_tweet_id)
+
+
+    def test_tweet_detail_api_view(self):
+        client = self.get_client()
+        response = client.get("/api/tweets/2/")
+        # print("DETAIL response", response)
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        _id = data.get("id")
+        self.assertEqual(_id, 2)
+
+    def test_tweet_delete_api_view(self):
+        self.client.force_authenticate(self.user)
+        response = self.client.delete("/api/tweets/1/delete/")
+        self.assertEqual(response.status_code, 200)
+        self.client.force_authenticate(self.user)
+        response = self.client.delete("/api/tweets/1/delete/")
+        self.assertEqual(response.status_code, 404)
+        self.client.force_authenticate(self.user)
+        response_incorrect_owner = self.client.delete("/api/tweets/3/delete/")
+        self.assertEqual(response_incorrect_owner.status_code, 401)
