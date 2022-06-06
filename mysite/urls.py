96b228322c31470bce2d1439d73cea6759ca268c
River <riveriswild@icloud.com>
2022-06-06 22:35:35 +0300

    React added

--- mysite/urls.py
+++ mysite/urls.py
@@ -23,7 +23,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view),
-    path('react/', TemplateView.as_view(template_name='react.html')),
+    path('react/', TemplateView.as_view(template_name='react_via_dj.html')),
     path('tweets/', tweet_list_view),
     path('tweets/<int:tweet_id>', tweet_detail_view),
     # path('api/tweets/action', tweet_action_view),
